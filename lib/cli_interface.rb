1aefd173af9244da859baa49979fbefffb389079
emilioquintana90 <emilioquintana90@gmail.com>
2020-05-27 17:26:25 -0500

    Add new GUI

--- lib/cli_interface.rb
+++ lib/cli_interface.rb
@@ -1,9 +1,13 @@
 require 'tty-prompt'
 
+def display_stats(user)
+  user.account.reload
+  puts "🧍‍♂️  #{user.name}  |  💰  #{user.account.balance}  \n\n"
+end
 
 def menu(user)
   puts `clear`
-  puts "Welcome #{user.name}"
+  display_stats(user)
   # $table.render(:basic)
   choices = %w(Deposit Withdraw History Cancel Balance Quit)
   action = $prompt.select("What would you like to do?", choices)
@@ -12,50 +16,55 @@ def menu(user)
   
       case action
         when "Deposit"
+          puts `clear`
+          display_stats(user)
           deposit(user)
         
         when "Withdraw"
+          puts `clear`
+          display_stats(user)
           puts "How much would you like to withdraw?"
           print ">>> "
           amount = gets.chomp.to_f
   
-          puts "Creating Transaction..."
           tr = Transfer.create(user_id: user.id, account_id:  user.account.id, amount: amount)
           tr.withdraw
 
         when "History"
-          puts "Which transactions would you like to view? All or Self"
-          user_input = gets.chomp
-          if user_input == "All"
+          puts `clear`
+          display_stats(user)
+
+          choices = %w(All\ Transactions My\ Transactions)
+          user_input = $prompt.select("Which transactions would you like to view?", choices)
+
+          if user_input == "All Transactions"
             user.transfers.map do |transfer|
-              puts "Transaction ID #{transfer.id}"
-              puts "Transaction Amount #{transfer.amount}"
+              puts "Transaction ID #{transfer.id} - Amount:  $ #{transfer.amount}"
             end
-          elsif user_input == "Self"
+          elsif user_input == "My Transactions"
             user.owned_transfers.map do |transfer|
-              puts "Transaction ID #{transfer.id}"
-              puts "Transaction Amount #{transfer.amount}"
+              puts "Transaction ID #{transfer.id} - Amount:  $ #{transfer.amount}"
             end
           end
         when "Cancel"
-          puts "Displaying Transactions..."
-          user.transfers.map do |transaction|
+          puts `clear`
+          display_stats(user)
+        
+          choices = user.transfers.map do |transaction|
             account = Account.find(transaction.account_id)
-            user = User.find(account.user_id)
-            puts "Name: #{user.name}"
-            puts "Transaction ID: #{transaction.id}"
-            puts "Amount: #{transaction.amount}"
+            reciever = User.find(account.user_id)
+            "#{reciever.name} - $ #{transaction.amount} - #{transaction.id}"
           end
 
-          puts "Please enter the ID of the transaction you would like to cancel:"
-          transaction_id = gets.chomp.to_i
-
+          user_input = $prompt.select("Which transactions would you like to cancel?", choices)
+          transaction_id = user_input.split()[-1].to_i
           transfer = Transfer.find(transaction_id)
-          puts "Reversing Transaction...."
           transfer.reverse_transfer
-        
+          user.transfers.reload
           puts "Transaction Deleted and Reversed Successfully."
         when "Balance"
+          puts `clear`
+          display_stats(user)
           user.account.reload
           balance = user.account.balance
           puts "Your current balance is $#{balance}"
@@ -69,25 +78,29 @@ def menu(user)
 end
 
 def deposit(user)
-  puts `clear`
-  choices = %w(MyAccount DifferentAccount) 
+  choices = %w(My\ Account Different\ Account) 
   choice = $prompt.select("Where would you like to deposit?", choices)
-    if choice == "MyAccount"
+    if choice == "My Account"
+      puts `clear`
+      display_stats(user)
       puts "How much would you like to deposit?"
       print ">>> "
       amount = gets.chomp.to_f 
-      puts "Creating Transaction..."
       tr = Transfer.create(user_id: user.id, account_id:  user.account.id, amount: amount)
       tr.deposit
-    elsif choice == "DifferentAccount"
-
+    elsif choice == "Different Account"
+      puts `clear`
+      display_stats(user)
       choices = Account.all.map do |account|
         "#{account.user.name}  - #{account.id}"
       end
 
       chosen_account = $prompt.select("Which account would you like to deposit to?", choices)
-      account_id = chosen_account[-1].to_i
+      account_id = chosen_account.split()[-1].to_i
       receiver = User.find(account_id)
+
+      puts `clear`
+      display_stats(user)
       puts "How much would you like to deposit?"
       print ">>> "
       amount = gets.chomp.to_f
